076b43d36a362b8a972311de2a94089f2199e427
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-02-25 17:34:56 +0530

    refactor: implement catchAsync utility for error handling in blog controller to catch errors in async operations

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,5 +1,6 @@
 const APIFeatures = require("./../utils/apiFeatures");
 const Blog = require("./../models/blogModel");
+const catchAsync = require("./../utils/catchAsync");
 
 exports.checkBlogId = (req, res, next, val) => {
   next();
@@ -10,96 +11,61 @@ exports.getLatestBlogs = (req, res, next) => {
   next();
 };
 
-exports.getAllBlogs = async (req, res) => {
-  try {
-    //! INITIAL QUERY
-    const features = new APIFeatures(Blog.find(), req.query)
-      .filter()
-      .limitFields()
-      .paginate()
-      .sort();
-    //! PROCESSING THE QUERY
-    const blogs = await features.query;
+exports.getAllBlogs = catchAsync(async (req, res, next) => {
+  //! INITIAL QUERY
+  const features = new APIFeatures(Blog.find(), req.query)
+    .filter()
+    .limitFields()
+    .paginate()
+    .sort();
+  //! PROCESSING THE QUERY
+  const blogs = await features.query;
 
-    res.status(200).json({
-      status: "success",
-      result: blogs.length,
-      data: {
-        blogs,
-      },
-    });
-  } catch (err) {
-    return res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    result: blogs.length,
+    data: {
+      blogs,
+    },
+  });
+});
 
-exports.getBlog = async (req, res) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
-    res.status(200).json({
-      status: "success",
-      data: {
-        blog,
-      },
-    });
-  } catch (err) {
-    return res.status(400).json({
-      status: "fail",
-      message: "err in getting the blog with given id",
-    });
-  }
-};
+exports.getBlog = catchAsync(async (req, res, next) => {
+  const blog = await Blog.findById(req.params.id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      blog,
+    },
+  });
+});
 
-exports.createBlog = async (req, res) => {
-  try {
-    const newBlog = await Blog.create(req.body);
-    res.status(200).json({
-      status: "success",
-      data: {
-        blog: newBlog,
-      },
-    });
-  } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: error,
-    });
-  }
-};
+exports.createBlog = catchAsync(async (req, res, next) => {
+  const newBlog = await Blog.create(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      blog: newBlog,
+    },
+  });
+});
 
-exports.updateBlog = async (req, res) => {
-  try {
-    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
-      runValidators: true,
-      new: true,
-    });
+exports.updateBlog = catchAsync(async (req, res, next) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  });
 
-    res.status(200).json({
-      status: "success",
-      data: updatedBlog,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: updatedBlog,
+  });
+});
 
-exports.deleteBlog = async (req, res) => {
-  try {
-    await Blog.findByIdAndDelete(req.params.id);
+exports.deleteBlog = catchAsync(async (req, res, next) => {
+  await Blog.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: "success",
-    });
-  } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: error,
-    });
-  }
-};
+  res.status(204).json({
+    status: "success",
+  });
+});
